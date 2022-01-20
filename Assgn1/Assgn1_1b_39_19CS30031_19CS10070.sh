20074c1ab1476cf4806137c2f8b7eb73e61844a1
JhadotAnimesh <jha.animesh01@gmail.com>
2022-01-20 19:17:52 +0530

    syncc

--- Assgn1/Assgn1_1b_39_19CS30031_19CS10070.sh
+++ Assgn1/Assgn1_1b_39_19CS30031_19CS10070.sh
@@ -1,3 +1,3 @@
 cp -R 1.b.files 1.b.files.out
-find 1.b.files.out -f -exec sort {} -n -o {} \;
-sort -n -m 1.b.files.out/*.txt | uniq -c | rev > 1.b.files.out.txt
+find 1.b.files.out -type f -exec sort {} -n -o{} \;
+sort -n -m 1.b.files.out/*.txt|uniq -c|rev>1.b.files.out.txt
