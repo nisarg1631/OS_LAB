0c9abed08fb38e3579cfdbae0b0d557b5ba831ea
Nisarg Upadhyaya <nisarg1631@gmail.com>
2022-01-21 16:26:53 +0530

    complete part g

--- Assgn1/Assgn1_1g_39_19CS30031_19CS10070.sh
+++ Assgn1/Assgn1_1g_39_19CS30031_19CS10070.sh
@@ -1,1 +1,1 @@
-for i in {1..10};do for j in {1..10};do printf "%d" "$(($RANDOM%30000-15000))">>ok.txt&&(($j==10))||printf ,>>ok.txt;done;printf '\n'>>ok.txt;done
+for i in {1..150};do for j in {1..10};do printf "%d" "$(($RANDOM%30000-15000))"&&(($j==10))||printf ,;done;printf '\n';done>$1;((`awk -F, '{print$'$2'}' $1|grep -c "$3"`>0))&&echo "YES"||echo "NO"
