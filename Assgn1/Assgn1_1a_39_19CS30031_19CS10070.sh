32c8f651c475d14724f7a269d76dd6a0aa47cb8a
Nisarg Upadhyaya <nisarg1631@gmail.com>
2022-01-21 14:52:35 +0530

    reduce code length and add part g

--- Assgn1/Assgn1_1a_39_19CS30031_19CS10070.sh
+++ Assgn1/Assgn1_1a_39_19CS30031_19CS10070.sh
@@ -1,1 +1,1 @@
-factor $1 | sed s/"$1: "//
+num=$1;for i in $(seq $1);do while :;do(($i!=1&&$num%$i==0))||break&&echo $i&&num=$(($num/$i));done;done
--- Assgn1/Assgn1_1d_39_19CS30031_19CS10070.sh
+++ Assgn1/Assgn1_1d_39_19CS30031_19CS10070.sh
@@ -1,2 +1,2 @@
 mkdir -p files_mod
-sed 's/[[:blank:]]\+/,/g' $1 | nl > files_mod/$(basename $1)
+sed 's/[[:blank:]]\+/,/g' $1|nl>files_mod/$(basename $1)
