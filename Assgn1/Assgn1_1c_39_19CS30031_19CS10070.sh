60fc3420a511652ddff53b62b62442cfa4688762
Nisarg Upadhyaya <nisarg1631@gmail.com>
2022-01-23 23:02:26 +0530

    c final

--- Assgn1/Assgn1_1c_39_19CS30031_19CS10070.sh
+++ Assgn1/Assgn1_1c_39_19CS30031_19CS10070.sh
@@ -1,4 +1,1 @@
-mkdir -p data/Nil
-find data -type f -not -name "*.*" -not -path "data/Nil/*" -exec mv -t data/Nil {} +
-find data -type f -not -path "data/Nil/*"|awk -F. '{print$(NF)}'|sort -u|xargs -I% sh -c 'mkdir -p data/%;find data -type f -name "*.%" -not -path "data/%/*" -exec mv -t data/% {} +;'
-find data -type d -empty -delete
+cmd="find data -type f -not -path";mkdir -p data/Nil;$cmd 'data/Nil/*' -not -name '*.*' -exec mv -t data/Nil {} +;$cmd 'data/Nil/*'|awk -F. '{print$(NF)}'|sort -u|xargs -I% sh -c "mkdir -p data/%;$cmd 'data/%/*' -name '*.%' -exec mv -t data/% {} +;";find data -type d -empty -delete
