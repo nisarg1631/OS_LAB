f7536f61dac375190a75bedddb7584d9b2893553
Nisarg Upadhyaya <nisarg1631@gmail.com>
2022-02-14 20:22:02 +0530

    task2 done

--- Assgn3/task2.cpp
+++ Assgn3/task2.cpp
@@ -12,20 +12,28 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
+#include <chrono>
+#include <random>
+#include <time.h>
 using namespace std;
 
-const int MAT_SIZE = 100;
+const int MAT_SIZE = 1000;
 const int MAX_QUEUE_SIZE = 10;
 const int RANDOM_ID_RANGE_LO = 1;
 const int RANDOM_ID_RANGE_HI = 100000;
+const int MATRIX_ELEMENT_LO = -9;
+const int MATRIX_ELEMENT_HI = 9;
+const int WAIT_TIME_LO = 0;
+const int WAIT_TIME_HI = 3000;
 
 int total_jobs;
+random_device rd;
+mt19937 generator(rd());
 
 vector<vector<int>> get_random_vectors(int cnt) {
-    srand(time(0));
     vector<int> v(RANDOM_ID_RANGE_HI - RANDOM_ID_RANGE_LO + 1);
     iota(v.begin(), v.end(), 1);
-    random_shuffle(v.begin(), v.end());
+    shuffle(v.begin(), v.end(), generator);
     vector<vector<int>> r(cnt);
     int it = 0;
     while(!v.empty()) {
@@ -36,13 +44,23 @@ vector<vector<int>> get_random_vectors(int cnt) {
     return r;
 }
 
+int get_random_matrix_entry() {
+    static uniform_int_distribution<int> range(MATRIX_ELEMENT_LO, MATRIX_ELEMENT_HI);
+    return range(generator);
+}
+
+int get_random_wait_time() {
+    static uniform_int_distribution<int> range(WAIT_TIME_LO, WAIT_TIME_HI);
+    return range(generator);
+}
+
 // FORWARD DECLARATIONS
 
 // Shared mutex - INSPIRED BY : https://gist.github.com/yamnikov-oleg/abf61cf96b4867cbf72d
 struct shared_mutex {
     pthread_mutex_t *mutex_ptr;
     void init();
-} *sm_updatequeue, *sm_updatemat, *sm_jobscount;
+} *sm_updatequeue, *sm_updatemat, *sm_jobscount, *sm_output;
 
 struct comp_job {
     int producer_number, matrix_id, status, result_index;
@@ -68,14 +86,26 @@ struct comp_job {
     comp_job(int, int);
 };
 
+struct worker_task {
+    int result_index, block_flag;
+    // Matrix Block 1
+    int block_A[MAT_SIZE/2][MAT_SIZE/2];
+    int matrix1_id, matrix1_creator;
+    // Matrix Block 2
+    int block_B[MAT_SIZE/2][MAT_SIZE/2];
+    int matrix2_id, matrix2_creator;
+    // Block Product
+    int block_product[MAT_SIZE/2][MAT_SIZE/2];
+};
+
 struct job_queue {
     comp_job jobs[MAX_QUEUE_SIZE];
     int count, front, back;
 
     void init();
-    bool fetch(int [][MAT_SIZE/2], int [][MAT_SIZE/2], int &, int &, vector<int> &, int);
-    void update(int [][MAT_SIZE/2], int, int, int);
-    int push(comp_job, int);
+    bool fetch(worker_task &, vector<int> &, int);
+    void update(worker_task &, int);
+    int push(const comp_job &, int);
     bool pop();
 };
 
@@ -102,12 +132,16 @@ void job_queue::init() {
     count = front = back = 0;
 }
 
-bool job_queue::fetch(int block_A[MAT_SIZE/2][MAT_SIZE/2], int block_B[MAT_SIZE/2][MAT_SIZE/2], int &block_flag, int &result_index, vector<int> &random_ids, int worker_number) {
+bool job_queue::fetch(worker_task &task, vector<int> &random_ids, int worker_number) {
     bool success = false;
     pthread_mutex_lock(sm_updatequeue->mutex_ptr);
     if(master->jobs_created == total_jobs && count == 1) {
         pthread_mutex_unlock(sm_updatequeue->mutex_ptr);
-        printf("Worker with number %d terminated\n", worker_number);
+        #ifndef SUPPRESS_OUTPUT
+            pthread_mutex_lock(sm_output->mutex_ptr);
+            printf("\n -> Worker with number %d terminated. PID: %d\n", worker_number, getpid());
+            pthread_mutex_unlock(sm_output->mutex_ptr);
+        #endif
         exit(0);
     }
     if(count > 1) {
@@ -121,19 +155,56 @@ bool job_queue::fetch(int block_A[MAT_SIZE/2][MAT_SIZE/2], int block_B[MAT_SIZE/
                 if((jobs[front].status & (1 << flag)) == 0)
                     break;
             }
-            block_flag = flag;
+            task.block_flag = flag;
             jobs[front].status |= (1 << flag);
-            // TODO: Assign block_A and block_B here
-            int i = flag & (1 << 2);
-            int j = flag & (1 << 1);
-            int k = flag & (1 << 0);
+            // Assign task.block_A and task.block_B here
+            int i = (flag >> 2) & 1;
+            int j = (flag >> 1) & 1;
+            int k = (flag >> 0) & 1;
+            int row_it = 0, col_it = 0;
+            for(int row = i * (MAT_SIZE/2); row < (i + 1) * (MAT_SIZE/2); row++) {
+                for(int col = k * (MAT_SIZE/2); col < (k + 1) * (MAT_SIZE/2); col++) {
+                    task.block_A[row_it][col_it++] = jobs[front].matrix[row][col];
+                }
+                col_it = 0;
+                row_it++;
+            }
+            row_it = 0, col_it = 0;
+            for(int row = k * (MAT_SIZE/2); row < (k + 1) * (MAT_SIZE/2); row++) {
+                for(int col = j * (MAT_SIZE/2); col < (j + 1) * (MAT_SIZE/2); col++) {
+                    task.block_B[row_it][col_it++] = jobs[(front + 1) % MAX_QUEUE_SIZE].matrix[row][col];
+                }
+                col_it = 0;
+                row_it++;
+            }
             if(flag == 0) {
                 // First access, push a new matrix to queue
-                comp_job cj(-1, random_ids.back());
+                comp_job cj(-worker_number, random_ids.back());
                 random_ids.pop_back();
                 while((jobs[front].result_index = push(cj, 0)) == -1) ; // wait until push is successful, if everything works right it will push on the first try itself
             }
-            result_index = jobs[front].result_index;
+            task.result_index = jobs[front].result_index;
+            task.matrix1_id = jobs[front].matrix_id;
+            task.matrix1_creator = jobs[front].producer_number;
+            task.matrix2_id = jobs[(front + 1) % MAX_QUEUE_SIZE].matrix_id;
+            task.matrix2_creator = jobs[(front + 1) % MAX_QUEUE_SIZE].producer_number;
+            #ifndef SUPPRESS_OUTPUT
+                pthread_mutex_lock(sm_output->mutex_ptr);
+                printf("\n ---- WORKER FETCHED BLOCKS ----\n");
+                printf(" -> Worker number: %d\n", worker_number);
+                printf(" -> PID: %d\n", getpid());
+                printf(" -> Matrix 1 details:\n");
+                printf("    [ Matrix ID: %d ]\n", task.matrix1_id);
+                printf("    [ Block fetched: %d%d ]\n", i, k);
+                printf("    [ Added by: %s %d ]\n", (task.matrix1_creator > 0 ? "Producer" : "Worker"), abs(task.matrix1_creator));
+                printf(" -> Matrix 2 details:\n");
+                printf("    [ Matrix ID: %d ]\n", task.matrix2_id);
+                printf("    [ Block fetched: %d%d ]\n", k, j);
+                printf("    [ Added by: %s %d ]\n", (task.matrix2_creator > 0 ? "Producer" : "Worker"), abs(task.matrix2_creator));
+                printf(" -> Work done: Read blocks\n");
+                printf(" -------------------------------\n");
+                pthread_mutex_unlock(sm_output->mutex_ptr);
+            #endif
             if(flag == 7) {
                 // Last access, remove A and B from queue
                 pop();
@@ -146,33 +217,73 @@ bool job_queue::fetch(int block_A[MAT_SIZE/2][MAT_SIZE/2], int block_B[MAT_SIZE/
     return success;
 }
 
-void job_queue::update(int block_product[MAT_SIZE/2][MAT_SIZE/2], int block_flag, int result_index, int worker_number) {
+void job_queue::update(worker_task &task, int worker_number) {
     pthread_mutex_lock(sm_updatemat->mutex_ptr);
-    // TODO: Modify C_ij (jobs[result_index].matrix) here
-    block_flag >>= 1;
-    int i = block_flag & (1 << 1);
-    int j = block_flag & (1 << 0);
-    if((jobs[result_index].status & (1 << (8 + block_flag))) == 0) {
-        // First modification to C_ij, so copy the block_product to C_ij
-        jobs[result_index].status |= (1 << (8 + block_flag));
+    // Modify C_ij (jobs[task.result_index].matrix) here
+    int i = (task.block_flag >> 2) & 1;
+    int j = (task.block_flag >> 1) & 1;
+    int k = (task.block_flag >> 0) & 1;
+    task.block_flag >>= 1;
+    bool copy_task = false;
+    if((jobs[task.result_index].status & (1 << (8 + task.block_flag))) == 0) {
+        copy_task = true;
+        // First modification to C_ij, so copy the task.block_product to C_ij
+        int row_it = 0, col_it = 0;
+        for(int row = i * (MAT_SIZE/2); row < (i + 1) * (MAT_SIZE/2); row++) {
+            for(int col = j * (MAT_SIZE/2); col < (j + 1) * (MAT_SIZE/2); col++) {
+                jobs[task.result_index].matrix[row][col] = task.block_product[row_it][col_it++];
+            }
+            col_it = 0;
+            row_it++;
+        }
+        jobs[task.result_index].status |= (1 << (8 + task.block_flag));
     } else {
-        // Second modification to C_ij, so add the block_product to C_ij
-        jobs[result_index].status |= (1 << (12 + block_flag));
+        // Second modification to C_ij, so add the task.block_product to C_ij
+        int row_it = 0, col_it = 0;
+        for(int row = i * (MAT_SIZE/2); row < (i + 1) * (MAT_SIZE/2); row++) {
+            for(int col = j * (MAT_SIZE/2); col < (j + 1) * (MAT_SIZE/2); col++) {
+                jobs[task.result_index].matrix[row][col] += task.block_product[row_it][col_it++];
+            }
+            col_it = 0;
+            row_it++;
+        }
+        jobs[task.result_index].status |= (1 << (12 + task.block_flag));
     }
+    #ifndef SUPPRESS_OUTPUT
+        pthread_mutex_lock(sm_output->mutex_ptr);
+        printf("\n ---- WORKER WRITING BLOCK ----\n");
+        printf(" -> Worker number: %d\n", worker_number);
+        printf(" -> PID: %d\n", getpid());
+        printf(" -> Matrix 1 details:\n");
+        printf("    [ Matrix ID: %d ]\n", task.matrix1_id);
+        printf("    [ Block fetched: %d%d ]\n", i, k);
+        printf("    [ Added by: %s %d ]\n", (task.matrix1_creator > 0 ? "Producer" : "Worker"), abs(task.matrix1_creator));
+        printf(" -> Matrix 2 details:\n");
+        printf("    [ Matrix ID: %d ]\n", task.matrix2_id);
+        printf("    [ Block fetched: %d%d ]\n", k, j);
+        printf("    [ Added by: %s %d ]\n", (task.matrix2_creator > 0 ? "Producer" : "Worker"), abs(task.matrix2_creator));
+        printf(" -> Work done: %s\n", (copy_task ? "Copy block" : "Add block"));
+        printf(" -> Resultant matrix details:\n");
+        printf("    [ Matrix ID: %d ]\n", jobs[task.result_index].matrix_id);
+        printf("    [ Block modified: %d%d ]\n", i, j);
+        printf("    [ Added by: %s %d ]\n", (jobs[task.result_index].producer_number > 0 ? "Producer" : "Worker"), abs(task.matrix1_creator)); // If everything works alright this should always print a Worker
+        printf(" ------------------------------\n");
+        pthread_mutex_unlock(sm_output->mutex_ptr);
+    #endif
     int done = 1;
     for(int i=12; i<16; i++) {
-        if((jobs[result_index].status & (1 << i)) == 0) {
+        if((jobs[task.result_index].status & (1 << i)) == 0) {
             done = 0;
             break;
         }
     }
     // If all blocks completed set ready bit to 1
     if(done)
-        jobs[result_index].status |= (1 << 31);
+        jobs[task.result_index].status |= (1 << 31);
     pthread_mutex_unlock(sm_updatemat->mutex_ptr);
 }
 
-int job_queue::push(comp_job cj, int is_producer) {
+int job_queue::push(const comp_job &cj, int is_producer) {
     int success = -1;
     pthread_mutex_lock(sm_updatequeue->mutex_ptr);
     if(count < MAX_QUEUE_SIZE - is_producer) {
@@ -180,6 +291,15 @@ int job_queue::push(comp_job cj, int is_producer) {
         jobs[back] = cj;
         success = back;
         back = (back + 1) % MAX_QUEUE_SIZE;
+        #ifndef SUPPRESS_OUTPUT
+            pthread_mutex_lock(sm_output->mutex_ptr);
+            printf("\n ---- JOB ADDED TO QUEUE ----\n");
+            printf(" -> Added by: %s %d\n", (is_producer ? "Producer" : "Worker"), (is_producer ? cj.producer_number : -cj.producer_number));
+            printf(" -> PID: %d\n", getpid());
+            printf(" -> Matrix ID: %d\n", cj.matrix_id);
+            printf(" ----------------------------\n");
+            pthread_mutex_unlock(sm_output->mutex_ptr);
+        #endif
     }
     pthread_mutex_unlock(sm_updatequeue->mutex_ptr);
     return success;
@@ -203,49 +323,88 @@ void shared_memory::init() {
 }
 
 void producer(const int producer_number, vector<int> random_ids) {
-    printf("Producer with number %d created\n", producer_number);
+    #ifndef SUPPRESS_OUTPUT
+        pthread_mutex_lock(sm_output->mutex_ptr);
+        printf("\n -> Producer with number %d created. PID: %d\n", producer_number, getpid());
+        pthread_mutex_unlock(sm_output->mutex_ptr);
+    #endif
     while(1) {
         comp_job cj(producer_number, random_ids.back());
         cj.status |= (1 << 31);
         random_ids.pop_back();
-        // TODO: Generate random matrix here
+        // Generate random matrix here
+        for(int i = 0; i < MAT_SIZE; i++) {
+            for(int j = 0; j < MAT_SIZE; j++) {
+                cj.matrix[i][j] = get_random_matrix_entry();
+                // uncomment to generate identity matrices and check if multiplication algorithm is working fine
+                // cj.matrix[i][j] = (i == j ? 1 : 0);
+            }
+        }
         pthread_mutex_lock(sm_jobscount->mutex_ptr);
         if(master->jobs_created == total_jobs) {
             pthread_mutex_unlock(sm_jobscount->mutex_ptr);
             break;
         }
         do {
-            // sleep(1); // TODO: Make this sleep random between 0 and 3000 ms
+            timespec sleepduration;
+            sleepduration.tv_nsec = 1000000L * (long)get_random_wait_time();
+            sleepduration.tv_sec = sleepduration.tv_nsec / 1000000000L;
+            sleepduration.tv_nsec %= 1000000000L;
+            nanosleep(&sleepduration, NULL);
         } while((master->queue).push(cj, 1) == -1); // wait until push is successful
         (master->jobs_created)++;
         pthread_mutex_unlock(sm_jobscount->mutex_ptr);
     }
-    printf("Producer with number %d terminated\n", producer_number);
+    #ifndef SUPPRESS_OUTPUT
+        pthread_mutex_lock(sm_output->mutex_ptr);
+        printf("\n -> Producer with number %d terminated. PID: %d\n", producer_number, getpid());
+        pthread_mutex_unlock(sm_output->mutex_ptr);
+    #endif
     exit(0);
 }
 
 void worker(const int worker_number, vector<int> random_ids) {
-    printf("Worker with number %d created\n", worker_number);
+    #ifndef SUPPRESS_OUTPUT
+        pthread_mutex_lock(sm_output->mutex_ptr);
+        printf("\n -> Worker with number %d created. PID: %d\n", worker_number, getpid());
+        pthread_mutex_unlock(sm_output->mutex_ptr);
+    #endif
     while(1) {
-        int block_A[MAT_SIZE/2][MAT_SIZE/2], block_B[MAT_SIZE/2][MAT_SIZE/2], block_flag, result_index;
+        worker_task task;
         do {
-            // sleep(1); // TODO: Make this sleep random between 0 and 3000 ms
-        } while(!(master->queue).fetch(block_A, block_B, block_flag, result_index, random_ids, worker_number)); // wait until two jobs are fetched
-        // TODO: Multiply block_A and block_B, store result in block_product
-        int block_product[MAT_SIZE/2][MAT_SIZE/2];
-        (master->queue).update(block_product, block_flag, result_index, worker_number);
+            timespec sleepduration;
+            sleepduration.tv_nsec = 1000000L * (long)get_random_wait_time();
+            sleepduration.tv_sec = sleepduration.tv_nsec / 1000000000L;
+            sleepduration.tv_nsec %= 1000000000L;
+            nanosleep(&sleepduration, NULL);
+        } while(!(master->queue).fetch(task, random_ids, worker_number)); // wait until two jobs are fetched
+        // Multiply task.block_A and task.block_B, store result in task.block_product
+        for(int i = 0; i < (MAT_SIZE/2); i++) {
+            for(int j = 0; j < (MAT_SIZE/2); j++) {
+                task.block_product[i][j] = 0;
+                for(int k = 0; k < (MAT_SIZE/2); k++) {
+                    task.block_product[i][j] += (task.block_A[i][k] * task.block_B[k][j]);
+                }
+            }
+        }
+        (master->queue).update(task, worker_number);
     }
     exit(0);
 }
 
 signed main() {
+    srand(time(0));
 
     int np, nw;
-    cin >> np >> nw;
+    cout << "Enter number of producers: ";
+    cin >> np;
+    cout << "Enter number of workers: ";
+    cin >> nw;
+    cout << "Enter total matrices to multiply: ";
     cin >> total_jobs;
 
     int shm_id;
-    if((shm_id = shmget(IPC_PRIVATE, (3 * sizeof(pthread_mutex_t)) + sizeof(shared_memory), IPC_CREAT | 0666)) == -1) {
+    if((shm_id = shmget(IPC_PRIVATE, (4 * sizeof(pthread_mutex_t)) + sizeof(shared_memory), IPC_CREAT | 0666)) == -1) {
         perror("Failed to create shared memory: ");
         exit(0);
     }
@@ -259,20 +418,26 @@ signed main() {
     sm_updatequeue = (shared_mutex *)malloc(sizeof(shared_mutex));
     sm_updatemat = (shared_mutex *)malloc(sizeof(shared_mutex));
     sm_jobscount = (shared_mutex *)malloc(sizeof(shared_mutex));
+    sm_output = (shared_mutex *)malloc(sizeof(shared_mutex));
 
     sm_updatequeue->mutex_ptr = (pthread_mutex_t *)shm_data;
     sm_updatemat->mutex_ptr = (sm_updatequeue->mutex_ptr) + 1;
     sm_jobscount->mutex_ptr = (sm_updatemat->mutex_ptr) + 1;
+    sm_output->mutex_ptr = (sm_jobscount->mutex_ptr) + 1;
 
-    master = (shared_memory *) ((sm_jobscount->mutex_ptr) + 1);
+    master = (shared_memory *) ((sm_output->mutex_ptr) + 1);
 
     sm_updatequeue->init();
     sm_updatemat->init();
     sm_jobscount->init();
+    sm_output->init();
 
     master->init();
 
     vector<vector<int>> random_vecs = get_random_vectors(np + nw);
+
+    auto start = chrono::high_resolution_clock::now();
+
     int it = 0;
     for(int i = 0; i < np; i++, it++) {
         int cpid = fork();
@@ -293,7 +458,18 @@ signed main() {
 
     while(wait(NULL) > 0) ;
 
-    cout << "Done" << endl;
+    auto stop = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(stop - start); 
+    cout << "\nTime taken: " << duration.count() << " milliseconds" << endl;
+
+    int ans = 0;
+    for(int i = 0; i < MAT_SIZE; i++) {
+        for(int j = 0; j < MAT_SIZE; j++) {
+            ans += (master->queue).jobs[(master->queue).front].matrix[i][j];
+        }
+    }
+
+    cout << "\nSum of the elements in the principal diagonal of the final matrix: " << ans << endl;
 
     shmdt(shm_data);
     shmctl(shm_id, IPC_RMID, NULL);
