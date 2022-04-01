f2309ee17a2a00df7cefecb60e1e8b9bf6ba326a
Animesh Jha <jha.animesh01@gmail.com>
2022-04-01 23:37:46 +0530

    finalish

--- Assgn5/run_test_not_for_profiling.sh
+++ Assgn5/run_test_not_for_profiling.sh
@@ -5,6 +5,6 @@
 # ./a.out 6
 # # g++  -std=c++17  -Wshadow  -Wall  -Wextra  -pedantic  -Wformat=2  -Wfloat-equal  -Wconversion  -Wlogical-op  -Wshift-overflow=2  -Wduplicated-cond  -Wcast-qual  -Wcast-align  -Wno-unused-result  -Wno-sign-conversion  -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=thread  -fno-sanitize-recover=all  -fstack-protector  -D_FORTIFY_SOURCE=2    -fsanitize=undefined  -D_GLIBCXX_DEBUG  -g  -DLMAO memlab.cpp memlab.cpp demo1.cpp  -o demo1 -lpthread
 # # ./demo1
-rm -f demo2
-g++ -std=c++17  -Wshadow  -Wall  -Wextra  -pedantic  -Wformat=2  -Wfloat-equal  -Wconversion  -Wlogical-op  -Wshift-overflow=2  -Wduplicated-cond  -Wcast-qual  -Wcast-align  -Wno-unused-result  -Wno-sign-conversion  -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=thread  -fno-sanitize-recover=all  -fstack-protector  -D_FORTIFY_SOURCE=2    -fsanitize=undefined  -D_GLIBCXX_DEBUG  -g  -DLMAO memlab.cpp demo2.cpp -lpthread 
-./a.out 6
+# rm -f demo2
+# g++ -std=c++17  -Wshadow  -Wall  -Wextra  -pedantic  -Wformat=2  -Wfloat-equal  -Wconversion  -Wlogical-op  -Wshift-overflow=2  -Wduplicated-cond  -Wcast-qual  -Wcast-align  -Wno-unused-result  -Wno-sign-conversion  -D_GLIBCXX_DEBUG_PEDANTIC -fsanitize=thread  -fno-sanitize-recover=all  -fstack-protector  -D_FORTIFY_SOURCE=2    -fsanitize=undefined  -D_GLIBCXX_DEBUG  -g  -DLMAO memlab.cpp demo2.cpp -lpthread 
+# ./a.out 6
